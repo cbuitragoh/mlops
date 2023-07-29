e8558635dd325bd9f998c7e6d1b05a9f1c38b04d
Carlos Buitrago <bhca0629@yahoo.es>
2023-07-29 15:26:22 -0500

    fixing stylistic rules

--- src/model/train.py
+++ src/model/train.py
@@ -11,7 +11,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
-
 # define functions
 def main(args):
     # TO DO: enable autologging
@@ -38,12 +37,14 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose',
+    X, y = df[['Pregnancies', 'PlasmaGlucose',
                'DiastolicBloodPressure',
-               'TricepsThickness','SerumInsulin',
-               'BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+               'TricepsThickness', 'SerumInsulin',
+               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=0.30,
+                                                        random_state=0)
 
     return (X_train, X_test, y_train, y_test)
 
@@ -68,6 +69,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
